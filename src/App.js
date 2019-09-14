7c8af4f511f9eacca821ddba6bd790d4ab93d55b
Constanza Sosa <constanza.sosa.g@gmail.com>
2019-09-14 18:16:27 +0200

    Done with phase-1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import data from './components/data'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userData: data
+    }
+  }
+
+  
+  render() {
+    return (
+      <div className="App">
+        <ul className="user__list">
+          {this.state.userData
+            .map ( item => {
+              return(
+                <li className="user">
+                  <h2>{item.name}</h2>
+                  <p>{item.email}</p>
+                  <ul>
+                    {item.passwords
+                    .map ( pass => {
+                      return(
+                      <li>
+                        {pass}
+                      </li>
+                      )} 
+                    )}
+                  </ul>
+                  <ul>
+                    <li>{item.bank.iban}</li>
+                    <li>{item.bank.pin}</li>
+                  </ul>
+                </li>
+            )}
+          )}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
